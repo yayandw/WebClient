832b9f6bf85250896f662448624eabbbae48fd4a
yayan <ya2n.wicaksono@gmail.com>
2025-06-10 17:45:51 +0700

    first

--- Personal/web/src/app/_components/writing.tsx
+++ Personal/web/src/app/_components/writing.tsx
@@ -4,20 +4,35 @@ import {useEffect, useState} from 'react';
 import ItemText from "@/app/_components/item_1";
 import Section from "@/app/_components/section";
 
+interface FeedItem {
+    title: string;
+    link: string;
+    pubDate: string;
+    ['content:encodedSnippet']: string;
+}
+
+interface Post {
+    year: string;
+    title: string;
+    description: string;
+    url: string;
+}
+
 export default function Writing() {
-    const [posts, setPosts] = useState([]);
+    const [posts, setPosts] = useState<Post[]>([]);
     useEffect(() => {
         fetch('/api/medium')
             .then(res => res.json())
-            .then(posts => {
-                const parsedPosts = posts.map(item => {
+            .then((posts: FeedItem[]) => {
+                const parsedPosts = posts.map((item: FeedItem) => {
                     const pubDate = new Date(item.pubDate);
-                    return {
+                    const post: Post = {
                         year: `${pubDate.getFullYear()}-${pubDate.getMonth() + 1}-${pubDate.getDate()}`,
                         title: item.title,
                         description: item["content:encodedSnippet"].substring(0, 500),
                         url: item.link
-                    };
+                    }
+                    return post;
                 });
                 setPosts(parsedPosts);
             });
